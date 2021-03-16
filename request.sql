9594162da2d092c5dd3ab8a43537a9e5311d1d7a
nym-jmorris <jmorris@nymets.com>
2021-03-16 13:11:12 -0400

    First commit. Kicking the tires on sql to github

--- request.sql
+++ request.sql
@@ -1,1 +1,11 @@
-select * from request
+select 
+
+g.EventCode
+, concat(ua.FirstName,' ', ua.LastName) rname
+, concat(r.RecipientFirstName,' ',+ r.RecipientLastName) recipient
+
+from request r
+join useraccount ua on r.requestor_id = ua.id
+join game g on g.id= r.game_id
+
+where g.is_archived =1
